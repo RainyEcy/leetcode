e81cb39ff86c537a7ddb2a4575a1c64042cfd9a0
shine.Rain <shinerainy_ycy@163.com>
2022-06-29 11:33:29 +0800

    动态规划

---
+++ 动态规划/src/三角形最短路径和/MinimumTotal.java
@@ -0,0 +1,56 @@
+package 三角形最短路径和;
+
+import java.util.List;
+
+/**
+ * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+ * <p>
+ * 每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+ * 输出：11
+ * 解释：如下面简图所示：
+ * 2
+ * 3 4
+ * 6 5 7
+ * 4 1 8 3
+ * 自顶向下的最小路径和为11（即，2+3+5+1= 11）。
+ * 示例 2：
+ * <p>
+ * 输入：triangle = [[-10]]
+ * 输出：-10
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -104 <= triangle[i][j] <= 104
+ * <p>
+ * 进阶：
+ * <p>
+ * 你可以只使用 O(n)的额外空间（n 为三角形的总行数）来解决这个问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinimumTotal {
+
+    /**
+     * 时间复杂度：O(N^2) N 为三角形的行数。
+     * 空间复杂度：O(N) N 为三角形的行数。
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) { // 列
+            for (int j = 0; j <= i; j++) { // 行
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
